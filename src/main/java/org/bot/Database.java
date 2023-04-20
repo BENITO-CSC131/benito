f8f77cc9fa205bfc327d81060a4eea2dcfcb2be3
ShadeManEXE <jflores99@rocketmail.com>
2023-04-19 19:57:40 -0700

    Added a hashmap idea for potential performance and less work on the methods

--- src/main/java/org/bot/Database.java
+++ src/main/java/org/bot/Database.java
@@ -3,6 +3,7 @@ package org.bot;
 import org.json.JSONArray;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
     //fields
@@ -14,6 +15,14 @@ public class Database {
     private ArrayList<Assignments> past;
     private ArrayList<Assignments> undated;
     private ArrayList<Assignments> overdue;
+
+    // idea of fast getting of courses since technically the amount of classes can can change from student to student and so Integer would be courseID and will make faster performance of getting courses.
+    private HashMap<Integer, Assignments> pastHASH;
+    private HashMap<Integer, Assignments> upcomingHASH;
+    private HashMap<Integer, Assignments> undatedHASH;
+    private HashMap<Integer, Assignments> overdueHASH;
+
+
     //TODO
     // had an idea that we could use a hash map for each ASS_ categories and to have key values of course objects (COURSE ID). Ill discuss more on the meet on saturday
 
