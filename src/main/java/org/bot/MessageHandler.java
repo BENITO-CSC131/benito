ed10ff10c138ce906e9203e291f8e19c45de1987
Lydell <52779496+LydellJones@users.noreply.github.com>
2023-04-29 23:44:19 -0700

    Don't Apologize

--- src/main/java/org/bot/MessageHandler.java
+++ src/main/java/org/bot/MessageHandler.java
@@ -1,135 +1,73 @@
 package org.bot;
 
+import java.util.ArrayList;
+
 public class MessageHandler
 {
-    public MessageHandler next;
-    private int size = 0;
-    private String incomingString;
+    public ArrayList<String> content = new ArrayList<>();
     private static final int MAX_CHAR_COUNT = 2000;
-    private static final String assFormat = "%d - %d - %s - %d";
-    private static final String courseFormat = "%d - %s";
-    private boolean atCapacity = false;
-
-    public MessageHandler(MessageHandler next)
-    {
-        this.next = next;
-    }
-
-    public MessageHandler(String s)
-    {
-        //this constructor needs to take one of two things
-        //the overflow of the previous MessageHandler
-        //or
-        //a new mass string object
-        //NOTE: when grabbing the overflow, 
-        //if it overflows more than once then break apart the string
-        //link a new messagehandler then store the content recursively
-        
-        if (s.length() <= MAX_CHAR_COUNT)
-        {
-            this.incomingString = s;
-            this.size = s.length();
-        } 
-        else
-        {
-            this.atCapacity = true;
-            int startIndex = 0;
-            int endIndex = MAX_CHAR_COUNT;
-            while (endIndex < s.length()) 
-            {
-                String subStr = s.substring(startIndex, endIndex); //create substring
-                MessageHandler newHandler = new MessageHandler(subStr); //new object created with substring recurses this method
-                this.size += newHandler.getSize(); //unsure: check this one
-                this.linkNextHandler(newHandler); //link the next handler to this object
-                startIndex = endIndex; //start equal to 2000
-                endIndex += MAX_CHAR_COUNT; //endIndex is now 4000
-            }
-            String lastSubStr = s.substring(startIndex);
-            MessageHandler lastHandler = new MessageHandler(lastSubStr);
-            this.size += lastHandler.getSize();
-            this.linkNextHandler(lastHandler);
-        }
-    }
-
+    private static final String assFormat = "[%d]->[%d]: %s - %s\n";
+    private static final String courseFormat = "[%d]: %s\n";
+    private int currentindex = 0;
     private int getSize() { return 0; }
 
-    private void linkNextHandler(MessageHandler nextHandler)
+    public  MessageHandler()
     {
-        if (this.next == null) {
-            this.next = nextHandler;
-        } else {
-            this.next.linkNextHandler(nextHandler);
-        }
+        this.content.add("");
     }
 
-    private String formatCourse(String content, Course object) 
+    private String formatCourse(Course object)
     {
         //all messages need to be formatted using
-        String.format(courseFormat, object.getClass(), content);
-        return "";
+        return String.format(courseFormat, object.getCourseID(), object.getCourseName());
     }
 
-    private String formatAssignment(String content, Assignment object) 
+    private String formatAssignment(Assignment object)
     {
         //all messages need to be formatted using
-        String.format(assFormat, object.getAssName(), content);
-        return "";
+        return String.format(assFormat, object.getCourseID(), object.getAssID(), object.getAssName(), object.getAssDate());
     }    
-    
-    private MessageHandler newBuffer()
-    {
-        MessageHandler newHandler = new MessageHandler(incomingString);
-        this.next = newHandler;
-        return newHandler;
-    }
 
-    private void massLoad(String content)
-    {
-        //most likely makes use of newBuffer but roll over the string using a String class method using recursion
-        int length = content.length();
-        int start = 0;
-        while (start < length)
-        {
-            int end = Math.min(start + MAX_CHAR_COUNT, length);
-            String substring = content.substring(start, end);
-            start = end;
-        
-            if (incomingString == null) 
-            {
-                incomingString = substring;
-            } 
-            else 
-            {
-                incomingString += substring;
-            }
-            
-            if (incomingString.length() >= MAX_CHAR_COUNT)
-            {
-                newBuffer();
-            }
-        }
-    }
 
-    private String loadLine(String line)
-    {
-        //check that if you add the line it wont overflow if it does make a new buffer and set flag 
-        int lineLength = line.length();
-        if (size + lineLength > MAX_CHAR_COUNT)
+    private void courseToMessage(Course course) {
+        String content = formatCourse(course);
+
+        if ((content.length() + this.content.get(currentindex).length()) <= MAX_CHAR_COUNT)
         {
-            MessageHandler newBuffer = new MessageHandler(incomingString);
-            this.next = newBuffer;
-            return newBuffer.loadLine(line);
+            this.content.set(currentindex, this.content.get(currentindex).concat(content));
         }
         else
         {
-            incomingString += line;
-            size += lineLength;
-            if (size == MAX_CHAR_COUNT)
-            {
-                atCapacity = true;
-            }
-            return incomingString;
+            this.currentindex++;
+            this.content.add(content);
         }
-        // return "";
     }
+
+    private void assToMessage(Assignment assignment) {
+        String content = formatAssignment(assignment);
+
+        if ((content.length() + this.content.get(currentindex).length()) <= MAX_CHAR_COUNT)
+        {
+            this.content.set(currentindex, this.content.get(currentindex).concat(content));
+        }
+        else
+        {
+            this.currentindex++;
+            this.content.add(content);
+        }
+    }
+
+    public ArrayList<String> getAllMessages()
+    {
+        return this.content;
+    }
+
+    public String getByIndex(int index)
+    {
+        return this.content.get(index);
+    }
+
+
+
+
 }
